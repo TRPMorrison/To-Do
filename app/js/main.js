1f6f2d339c3a0cd9a8ea6082711f26a0843315f9
Trini Morrison <TRPMorrison@gmail.com>
2015-06-09 07:04:34 -0400

    DIVSHOT FILE REVISIONS TO ENTIRE PROJECT

---
+++ app/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(e) {
+
+$('#new-todo').dialog('open');
+  });
+
+
--- app/js/templates.js
+++ app/js/templates.js
@@ -1,8 +1,57 @@
-this["template"] = this["template"] || {};
-this["template"]["example"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    var helper;
+ // DIALOGUE BOX & SCRIPT
+ $('#new-todo').dialog({
+     modal: true,
+     autoOpen: false,
+     buttons: {
+         "Add task": function() {
+                 var taskName = $('#task').val();
+                 if (taskName === '') {
+                     return false;
+                 }
+                 var taskHTML = '<li><span class="done">%</span>';
+                 taskHTML += '<span class="delete">x</span>';
+                 taskHTML += '<span class="task"></span></li>';
+             }
+             // HTML TARGET
+         var $newTask = $(taskHTML);
 
-  return "<h1>\n  "
-    + this.escapeExpression(((helper = (helper = helpers.value || (depth0 != null ? depth0.value : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"value","hash":{},"data":data}) : helper)))
-    + "\n</h1>\n";
-},"useData":true});
+
+         $newTask.find('.task').text(taskName);
+
+
+         $newTask.hide();
+
+         $('#todo-list').prepend($newTask);
+
+
+         $newTask.show('clip', 250).effect('highlight', 1000);
+         $(this).dialog('close');
+     },
+     "Cancel": function() {
+         $(this).dialog('close');
+     }
+ },
+ close: function() {
+     $('#new-todo input').val('');
+ }
+ }); // END OF DIALOGUE BOX
+
+ // COMPLETED ITEMS
+ $('#todo-list').on('click', '.done', function() {
+     var $taskItem = $(this).parent('li');
+     $taskItem.slideUp(250, function() {
+         var $this = $(this);
+         $this.detach();
+         $('#completed-list').prepend($this);
+         $this.slideDown();
+     });
+ });
+ // END OF COMPLETED ITEMS
+
+
+
+ // DELETE TO DO
+ $('.sortlist').on('click', '.delete', function() {
+ $(this).parent('li').effect('explode', function() {
+     $(this).remove();
+ });
